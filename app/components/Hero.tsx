219f0cfc83d3ca55c18b549fdb9532e6e4791264
Rakesh Nagaraju <rakenju@gmail.com>
2025-05-07 21:06:18 -0700

    UPdate resume

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -91,7 +91,7 @@ const ActionButtons = memo(({ isMobile = false }: ActionButtonsProps) => (
     </motion.a>
     
     <motion.a
-      href="/files/Rakesh_Resume_May_2025.pdf"
+      href="/files/Rakesh_Resume_May_new_2025.pdf"
       download
       title="Download my CV"
       className={`
